17adde92a41009fe08e5016787370714fbd9dba1
felix <felixmariotto@gmail.com>
2020-03-17 16:08:48 +0100

    add info section

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { BrowserRouter } from 'react-router-dom' ;
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './fonts/Demagogue.otf'
+import './fonts/Demagogue.otf' ;
+import './fonts/ModernSans-Light.otf' ;
 
 ReactDOM.render(
 	<BrowserRouter>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,13 @@ export default class Home extends React.Component {
 		return (
 			<div>
 				<div className="info-container" >
-					hello from Home
+					<h2>Denounce your landlord</h2>
+					<br/>
+					<p>Did you get robbed of your deposit ?</p>
+					<p>You are not alone: every day in Hong Kong this kind of abuse occurs unpunished.</p>
+					<br />
+					<p>To keep somebody else from getting scammed by your landlord, you can denounce them here.
+					It will help future tenants making their choice.</p>
 				</div>
 			</div>
 		);
