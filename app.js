5e00f8c9d64ae5e77d3a141f48386dee177e7ca4
felix <felixmariotto@gmail.com>
2020-03-18 15:05:18 +0100

    create socket.io connection

--- app.js
+++ app.js
@@ -2,18 +2,40 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
-const app = express();
+const socketIO = require('socket.io');
+
+//
 
 console.log( 'production server' )
 
-app.use(express.static(path.join(__dirname, 'build')));
+//
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+const app = express()
+
+	.use(express.static(path.join(__dirname, 'build')))
+
+	.get('/ping', function (req, res) {
+	 return res.send('pong');
+	})
+
+	.get('/', function (req, res) {
+	  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	})
+
+	.listen(process.env.PORT || 8080)
+
+
+
+/////////////////
+//  SOCKET.IO
+/////////////////
+
+const io = socketIO( app );
+
+io.on('connection', (socket)=> {
+
+	console.log('client connected');
+
+	socket.emit( 'test', 'prout' );
+
 });
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(process.env.PORT || 8080);
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom' ;
 import './App.css';
+import socketIOClient from 'socket.io-client';
 
 import Home from './pages/Home' ;
 import Error from './pages/Error' ;
@@ -8,12 +9,17 @@ import Error from './pages/Error' ;
 import Navbar from './components/Navbar' ;
 
 export default class App extends React.Component {
-  
+
+	componentDidMount() {
+		console.log('mounted')
+		const socket = socketIOClient();
+		socket.on( "test", ()=> { console.log('message from server') } )
+	}
+
 	render() {
 
 		return (
 		    <div className="App">
-		    	
 		    	<Switch>
 		    		<Route exact path='/' component={ Home } />
 		    		<Route component={ Error } />
