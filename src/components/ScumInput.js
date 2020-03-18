04e5246c87f0da57c42740d965f9de42db6ca213
felix <felixmariotto@gmail.com>
2020-03-18 11:53:52 +0100

    update state and validate landlord's credentials

---
+++ src/components/ScumInput.js
@@ -0,0 +1,120 @@
+import React from 'react' ;
+import TextField from '@material-ui/core/TextField';
+
+export default class ScumInput extends React.Component {
+
+	constructor() {
+
+		super();
+
+		this.state = {
+			name: '',
+			address: '',
+			amount: '',
+			errorName: false,
+			errorAddress: false,
+			errorAmount: false
+		};
+
+		this.handleChange = this.handleChange.bind( this );
+		this.validateInput = this.validateInput.bind( this );
+
+		document.addEventListener( 'keydown', (e)=> {
+
+			if ( e.key === "Enter" ) {
+				this.validateInput();
+			};
+
+		});
+
+	};
+
+
+	validateInput() {
+
+		if ( this.state.name.length === 0 ) {
+			this.setState({
+				errorName: true
+			});
+		} else {
+			this.setState({
+				errorName: false
+			});
+		};
+
+		if ( this.state.address.length === 0 ) {
+			this.setState({
+				errorAddress: true
+			});
+		} else {
+			this.setState({
+				errorAddress: false
+			});
+		};
+
+		if ( this.state.amount.length === 0 ) {
+			this.setState({
+				errorAmount: true
+			});
+		} else {
+			this.setState({
+				errorAmount: false
+			});
+		};
+
+		if ( this.state.name.length !== 0 &&
+			 this.state.address.length !== 0 &&
+			 this.state.amount.length !== 0 ) {
+
+			console.log( 'input valid' );
+
+		};
+
+	};
+
+
+	handleChange(e) {
+
+		this.setState({
+			[ e.target.id ]: e.target.value
+		});
+
+	};
+
+
+	render() {
+
+		return (
+			<div className="input-container">
+				<div className="input-grid">
+					<div className="input-element">
+						<TextField
+							error={ this.state.errorName }
+							id="name"
+							label="Landlord's name"
+							onChange={ this.handleChange }
+						/>
+					</div>
+					<div className="input-element">
+						<TextField
+							error={ this.state.errorAddress }
+							id="address"
+							label='Address of the premises'
+							onChange={ this.handleChange }
+						/>
+					</div>
+					<div className="input-element">
+						<TextField
+							error={ this.state.errorAmount }
+							id="amount"
+							label='Amount of money robbed'
+							onChange={ this.handleChange }
+						/>
+					</div>
+				</div>
+			</div>
+		);
+
+	};
+
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react' ;
 import GeneralTable from '../components/Table' ;
+import ScumInput from '../components/ScumInput' ;
 
 export default class Home extends React.Component {
 	
@@ -15,8 +16,12 @@ export default class Home extends React.Component {
 					<br />
 					<p>To keep somebody else from getting scammed by your landlord, you can denounce them here.
 					It will help future tenants making their choice.</p>
+					<br />
+					<ScumInput />
 				</div>
 				<div className="info-container" >
+					<h2>Last scums denounced:</h2>
+					<br />
 					<GeneralTable />
 				</div>
 			</div>
